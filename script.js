deec01b38d1e79f45974dcca05af7bd1186d6393
Enrico Zappalà <zpp.enrico@gmail.com>
2018-08-31 19:25:41 +0200

    Add project files

---
+++ script.js
@@ -0,0 +1,16 @@
+var stl_viewer = new StlViewer ( document.getElementById("stl-container") );
+var volumeButton = document.getElementById("volume-button");
+var areaButton = document.getElementById("area-button");
+
+function calculateVolume() {
+  var volume = JSON.stringify(stl_viewer.get_model_info(1).volume);
+  document.getElementById("volume-output").innerHTML = volume;
+}
+
+function calculateArea() {
+  var area = JSON.stringify(stl_viewer.get_model_info(1).area);
+  document.getElementById("area-output").innerHTML = area;
+}
+
+volumeButton.addEventListener("click", calculateVolume);
+areaButton.addEventListener("click", calculateArea);

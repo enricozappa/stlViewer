b62e056be55a7402bc4ed0de320a37c118e634e8
Enrico Zappalà <zpp.enrico@gmail.com>
2018-09-03 09:03:16 +0200

    - Reduce renderer size - better code arrangement

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var stl_viewer = new StlViewer ( document.getElementById("stl-container") );
+var stl_viewer = new StlViewer(document.getElementById("stl-container"));
 var volumeButton = document.getElementById("volume-button");
 var areaButton = document.getElementById("area-button");
+
+function loadFile(file) {
+  stl_viewer.clean();
+  stl_viewer.add_model(
+    {
+      local_file: file
+    });
+}
 
 function calculateVolume() {
   var volume = JSON.stringify(stl_viewer.get_model_info(1).volume);
